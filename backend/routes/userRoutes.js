ce44da4bacc2c4a54fb3952c02545e8bf8d4f093
mdakbarhossain1 <akbarshovo18@gmail.com>
2024-03-17 19:23:55 +0600

    backend user done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile, deleteUserById } from "../controllers/userController.js";
+import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile, deleteUserById, getUserById, updateUserById } from "../controllers/userController.js";
 
 import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
 
@@ -14,11 +14,16 @@ router
 
 router.post('/auth', loginUser);
 router.post('/logout', logoutCurrentUser);
-router.route("/profile")
+router
+    .route("/profile")
     .get(authenticate, getCurrentUserProfile)
     .put(authenticate, updateCurrentUserProfile);
 
 // Admin Route for delete User 
-router.route("/:id").delete(authenticate, authorizedAdmin, deleteUserById);
+router
+    .route("/:id")
+    .delete(authenticate, authorizedAdmin, deleteUserById)
+    .get(authenticate, authorizedAdmin, getUserById)
+    .put(authenticate, authorizedAdmin, updateUserById)
 
 export default router;
